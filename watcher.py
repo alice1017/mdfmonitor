0e2290def1eb63a1eda9939e27033840fb21b940
alice <takemehighermore@gmail.com>
2014-03-19 01:32:45 +0900

    insert module docstring

--- watcher.py
+++ watcher.py
@@ -1,10 +1,56 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+"""watcher - watch the file modification
+
+The **watcher** is Python module about watching files modification using timestamp.
+
+
+## How to Use
+
+First, import this module.
+Second, create `Watcher` class's instance.
+Third, append file or files to instance.
+Fourth, using Python for sentence, You can write anything under for sentence.
+
+    #!/usr/bin/python
+    
+    import os
+    from watcher import Watcher
+    
+    files = os.listdir(".") # >>> ['sample.txt', 'sample.py']
+    
+    # create Watcher instnce
+    watcher = Watcher()
+    
+    # append file to watcher instance
+    watcher.add_file("sample.txt")
+    
+    # or 
+    
+    # append files to watcher instance
+    watcher.add_files(os.listdir("."))
+    
+    # start watch using with sentence
+    # you can write anything under for sentence.
+    # If watcher catch file modification, watcher run anything you written
+    for mdf in watcher.watch():
+        
+        print mdf.file.center(30, "=")
+        print "Catch the Modification!!"
+        print "Old timestamp: %s" % mdf.old_mtime
+        print "New timestamp: %s" % mdf.new_mtime
+        print "manager: %s" % str(mdf.manager.o_repository)
+        print "Diff".center(30,"=")
+        print mdf.diff
+"""
+
 import os
 import sys
 import time
 import difflib
+
+__version__ = "0.1.0b"
 
 class DuplicationError(BaseException):
     """Raise this Error if you add duplication file."""
