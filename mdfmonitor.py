c997f9e8b3e36b5f0b299bcfdfefa52787224baa
alice <takemehighermore@gmail.com>
2014-03-19 11:53:00 +0900

    rewrite docstring.

--- mdfmonitor.py
+++ mdfmonitor.py
@@ -3,52 +3,28 @@
 
 """
 mdfmonitor - Monitor the file moification
-###############################################
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 The **mdfmonitor** is Python module about monitoring files modification using timestamp and body.
 
-How to Use
-============
+How to use:
 
 It's simple. 
-
 1. Import this module.
 2. Create `ModificationMonitor` class's instance.
 3. Append file or files to instance using add_file method.
 4. using Python for sentence, You can write anything under for sentence.
 
-::
+    >>> from mdfmonitor import ModificationMonitor
+    >>> monitor = ModificationMonitor()
+    >>> monitor.add_file("README.md")
+    >>> for mdf in monitor.monitor():
+    ...     print "Old timestamp: %s" % mdf.old_mtime
+    ...     print "New timestamp: %s" % mdf.new_mtime
+    ...     print "manager: %s" % str(mdf.manager.o_repository)
+    ...     print "Diff".center(30,"=")
+    ...     print mdf.diff
 
-    #!/usr/bin/python
-
-    import os
-    from mdfmonitor import ModificationMonitor
-
-    files = os.listdir(".") # >>> ['sample.txt', 'sample.py']
-
-    # create Watcher instnce
-    monitor = Watcher()
-
-    # append file to mdfmonitor instance
-    monitor.add_file("sample.txt")
-
-    # or 
-
-    # append files to mdfmonitor instance
-    monitor.add_files(os.listdir("."))
-
-    # start watch using with sentence
-    # you can write anything under for sentence.
-    # If mdfmonitor catch file modification, mdfmonitor run anything you written
-    for mdf in monitor.monitor():
-
-        print mdf.file.center(30, "=")
-        print "Catch the Modification!!"
-        print "Old timestamp: %s" % mdf.old_mtime
-        print "New timestamp: %s" % mdf.new_mtime
-        print "manager: %s" % str(mdf.manager.o_repository)
-        print "Diff".center(30,"=")
-        print mdf.diff
 """
 
 import os
@@ -64,6 +40,13 @@ class DuplicationError(BaseException):
     pass
 
 class ModificationMonitor(object):
+
+    """The ModificationMonitor can monitoring file modification.
+
+    
+
+
+    """
 
     def __init__(self):
 
